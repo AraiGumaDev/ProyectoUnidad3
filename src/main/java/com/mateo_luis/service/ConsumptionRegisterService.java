c1932fa11fd114dd482cb67703eb02ed1bac74f2
LCarlosVZ <lcarlosvazap@gmail.com>
2023-10-15 21:51:58 -0500

    Se añadieron los metodos a la interface ConsumptionRegisterService

--- src/main/java/com/mateo_luis/service/ConsumptionRegisterService.java
+++ src/main/java/com/mateo_luis/service/ConsumptionRegisterService.java
@@ -1,4 +1,12 @@
 package com.mateo_luis.service;
 
 public interface ConsumptionRegisterService {
+
+    void intercambio();
+    void mediaDeConsumoPorVivienda();
+    void mediaDeConsumoPorHabitante(Double totalDeAguaConsumida);
+    void mediaDeHabitantesPorVivienda(Integer totalHabitantes);
+    void medianaDeHabitantes();
+    void modaDeHabitantesPorHogar();
+    void registrosDesactualizados();
 }
