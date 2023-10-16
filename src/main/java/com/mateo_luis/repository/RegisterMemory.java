f36822b2b36b8a1f9d2cc5bd7a54ecbe8dd322d2
LCarlosVZ <lcarlosvazap@gmail.com>
2023-10-15 21:46:56 -0500

    Se añadió el metodo llenarRegistroDeConsumoDeAgua a la interface RegisterMemory

--- src/main/java/com/mateo_luis/repository/RegisterMemory.java
+++ src/main/java/com/mateo_luis/repository/RegisterMemory.java
@@ -1,4 +1,10 @@
 package com.mateo_luis.repository;
 
+import com.mateo_luis.model.Register;
+
+import java.util.ArrayList;
+
 public interface RegisterMemory {
+
+    ArrayList<Register> llenarRegistroDeConsumoDeAgua();
 }
