6dcd24b887814ee010067b40fca8f21002fa338e
LCarlosVZ <lcarlosvazap@gmail.com>
2023-11-08 00:14:19 -0500

    Se eliminó la clase RegisterImpl y se corrigieron errores en la implementación de la interface

--- src/main/java/com/mateo_luis/repository/RegisterRepository.java
+++ src/main/java/com/mateo_luis/repository/RegisterRepository.java
@@ -3,8 +3,14 @@ package com.mateo_luis.repository;
 import com.mateo_luis.model.Register;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public interface RegisterRepository {
 
     ArrayList<Register> llenarRegistroDeConsumoDeAgua();
+
+    List<Register> findAllRegisters();
+    Optional<Register> getRegister(String direccion);
+    Register addRegister(Register newRegister)
 }
