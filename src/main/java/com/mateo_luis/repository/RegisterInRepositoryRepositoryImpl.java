115cbac1e463b39a50b8d58e77735f56cd2aacf5
AraiGumaDev <mateovargast@outlook.com>
2023-11-08 00:03:24 -0500

    Modificando lectura del txt

--- src/main/java/com/mateo_luis/repository/RegisterInRepositoryRepositoryImpl.java
+++ src/main/java/com/mateo_luis/repository/RegisterInRepositoryRepositoryImpl.java
@@ -19,7 +19,7 @@ import org.slf4j.LoggerFactory;
 import com.mateo_luis.model.Register;
 
 public class RegisterUsingFileRepositoryImpl implements RegisterRepository {
-    private static final Logger logger = LoggerFactory.getLogger( RegisterInRepositoryRepositoryImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger( RegisterUsingFileRepositoryImpl.class);
     private List<Register> registerList;
 
     public RegisterUsingFileRepositoryImpl() {
@@ -28,7 +28,7 @@ public class RegisterUsingFileRepositoryImpl implements RegisterRepository {
 
     private List<Register> loadRegisters(){
         logger.info( "Cargando los datos desde archivo" );
-        List<String> plainTextRegisterList =  readFileWithRegister();
+        List<String> plainTextRegisterList =  readFileWithRegisters();
         List<Register> registerList = plainTextRegisterList.stream().map( this::buildRegister ).toList();
         return registerList;
     }
@@ -44,8 +44,7 @@ public class RegisterUsingFileRepositoryImpl implements RegisterRepository {
         return Collections.emptyList();
     }
 
-    private Register buildRegister(String plainTextGrade){
-
+    private Register buildRegister(String plainTextRegister){
         String[] questionArray = plainTextRegister.split(",");
         Register register = new Register( questionArray[0], Integer.valueOf(questionArray[1]), Integer.valueOf(questionArray[2]), LocalDate.parse( questionArray[3], DateTimeFormatter.ofPattern("dd/MM/uuuu")), Double.valueOf(questionArray[4]));
 
@@ -58,8 +57,8 @@ public class RegisterUsingFileRepositoryImpl implements RegisterRepository {
     }
 
     @Override
-    public Optional<Register> getRegister(String unidad) {
-        return this.registerList.stream().filter( p->p.project().equals( unidad ) ).findAny();
+    public Optional<Register> getRegister(String direccion) {
+        return this.registerList.stream().filter( p->p.direccion().equals( direccion ) ).findAny();
     }
 
     @Override
@@ -73,9 +72,8 @@ public class RegisterUsingFileRepositoryImpl implements RegisterRepository {
     }
 
     private Predicate<Register> isTheRegisterOfTheProject(Register newRegister) {
-        return p -> p.project().equals( newRegister.project() );
+        return p -> p.direccion().equals( newRegister.direccion() );
     }
-
 
 
 
